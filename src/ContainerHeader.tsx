8d3002b2302fbe07a5c720873d04a8475d899522
s-kawabe <s.kawabe2281@gmail.com>
2021-12-11 23:34:22 +0900

    chore: fix directory

---
+++ src/ContainerHeader.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const ContainerHeader = () => {
+  return (
+    <div>
+      <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p>
+          Get started by editing{' '}
+          <code>pages/index.tsx</code>
+        </p>
+    </div>
+  )
+}
