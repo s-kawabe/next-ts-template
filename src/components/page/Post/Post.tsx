7485989b2f1ae3a5dae694bb3a19b3113aeae202
s-kawabe <s.kawabe2281@gmail.com>
2022-08-13 13:22:28 +0900

    remove husky & lint-staged

--- src/components/page/Post/Post.tsx
+++ src/components/page/Post/Post.tsx
@@ -1,7 +1,21 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import type { ChangeEvent } from 'react'
+import { useEffect, useState } from 'react'
 
 export const Post = () => {
+  const [text, setText] = useState('')
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }
+
+  useEffect(() => {
+    if (text.length >= 20) {
+      alert('text length for up to 20!')
+    }
+  }, [])
+
   return (
     <div>
       <Head>
@@ -11,6 +25,8 @@ export const Post = () => {
 
       <main>
         <h1>This is post page</h1>
+        <input type="text" onChange={handleChange} />
+        <p>{text}</p>
       </main>
       <Link href="/">back to top</Link>
     </div>
